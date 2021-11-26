da785ec0ac879b76506306bd70648080c9d220d4
jaivin <you@example.com>
2021-11-26 17:09:35 -0500

    Header finished

---
+++ Instagramclone/pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+     {/* header  */}
+     <Header/>
+     {/* feed   */}
+
+     {/* modal  */}
+
+    </div>
+  )
+}
